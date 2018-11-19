1e26ee5885ddf6df05dbf976265d5fbb8866dc94
VinylKnight <roger.bucio@loop.colum.edu>
2018-11-18 18:32:19 -0600

    Counting Collectables and Displaying Score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     [SerializeField]
     private Rigidbody2D playerRigidbody2D;
     [SerializeField]
     private float speed = 1f;
-    
+    [SerializeField]
+    private Text countText;
+    [SerializeField]
+    private Text winText;
 
-	void Update ()
+    private int collectableCount = 0;
+    void Start()
     {
-		
-	}
+        collectableCount = 0;
+        winText.text = "";
+        SetCountText();
+    }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -25,10 +32,22 @@ public class PlayerController : MonoBehaviour {
     {
         if (other.gameObject.CompareTag ("PickUp"))
         {
-           
+
             other.gameObject.SetActive(false);
+            collectableCount = collectableCount + 1;
+            SetCountText();
 
         }
 
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + collectableCount.ToString();
+        if (collectableCount >= 12)
+        {
+            winText.text = "You Win!!";
+
+        }
     }
 }
